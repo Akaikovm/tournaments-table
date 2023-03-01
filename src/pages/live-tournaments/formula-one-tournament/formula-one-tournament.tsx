9fdecb2a63c88dc81211fc1c9331b8eb245620b4
Sebastian <sebastianakaiko@gmail.com>
2023-03-01 17:06:14 -0500

    display text on tabs

--- src/pages/live-tournaments/formula-one-tournament/formula-one-tournament.tsx
+++ src/pages/live-tournaments/formula-one-tournament/formula-one-tournament.tsx
@@ -9,57 +9,18 @@ import {
 import { TitleBar } from "components/ui/titleBar";
 
 const Formula1Tournament = () => {
-  const data = [
-    {
-      label: "Reglas",
-      value: "reglas",
-      desc: `It really matters and then like it really doesn't matter.
-      What matters is the people who are sparked by it. And the people
-      who are like offended by it, it doesn't matter.`,
-    },
-    {
-      label: "Pilotos",
-      value: "pilotos",
-      desc: `Because it's about motivating the doers. Because I'm here
-      to follow my dreams and inspire other people to follow their dreams, too.`,
-    },
-
-    {
-      label: "Calendario",
-      value: "calendario",
-      desc: `We're not always in the position that we want to be at.
-      We're constantly growing. We're constantly making mistakes. We're
-      constantly trying to express ourselves and actualize our dreams.`,
-    },
-
-    {
-      label: "Resultados",
-      value: "resultados",
-      desc: `Because it's about motivating the doers. Because I'm here
-      to follow my dreams and inspire other people to follow their dreams, too.`,
-    },
-
-    {
-      label: "Estadisticas",
-      value: "estadisticas",
-      desc: `We're not always in the position that we want to be at.
-      We're constantly growing. We're constantly making mistakes. We're
-      constantly trying to express ourselves and actualize our dreams.`,
-    },
-  ];
-
   return (
     <>
       <div className="relative">
         <TitleBar title="Formula 1 2023" />
       </div>
       <Tabs id="custom-animation" value="reglas">
-        <TabsHeader className="text-red-700 cursor-pointer bg-primary">
-          {data.map(({ label, value }) => (
-            <Tab className="bg-gray-800" key={value} value={value}>
-              {label}
-            </Tab>
-          ))}
+        <TabsHeader className="text-red-700 cursor-pointer bg-gray-800">
+          <Tab value={"reglas"}>Reglas</Tab>
+          <Tab value={"pilotos"}>Pilotos</Tab>
+          <Tab value={"calendario"}>Calendario</Tab>
+          <Tab value={"resultados"}>Resultados</Tab>
+          <Tab value={"estadisticas"}>Estadisticas</Tab>
         </TabsHeader>
         <TabsBody
           animate={{
@@ -68,11 +29,92 @@ const Formula1Tournament = () => {
             unmount: { y: 250 },
           }}
         >
-          {data.map(({ value, desc }) => (
-            <TabPanel className="text-white" key={value} value={value}>
-              {desc}
-            </TabPanel>
-          ))}
+          <TabPanel className="text-white" value={"reglas"}>
+            <ul className="list-disc list-inside pl-3">
+              <li>
+                <span className="relative right-2">
+                  Los puntos seran repartidos de la misma manera que en la
+                  formula 1.
+                </span>
+              </li>
+              <li>
+                <span className="relative right-2">
+                  El formato de Clasificacion sera el mismo de la F1.
+                </span>
+              </li>
+              <li>
+                <span className="relative right-2">
+                  la IA correra con un nivel de dificultad de 65.
+                </span>
+              </li>
+              <li>
+                <span className="relative right-2">
+                  De no poderse crear el modo liga desde el juego por tener
+                  usuarios crossplay el torneo sera jugado en modo carrera
+                  amistosa con las normativas correspondientes.
+                </span>
+              </li>
+              <li>
+                <span className="relative right-2">
+                  Cualquier accidente de carrera podria ser estudiado y causar
+                  penalizacion para dicho piloto luego de cada carrera.
+                </span>
+              </li>
+              <li>
+                <span className="relative right-2">
+                  Luego de la 3era Vuelta cuando se active el DRS la carrera
+                  sera tomada como valida y no habra repeticiones.
+                </span>
+              </li>
+              <li>
+                <span className="relative right-2">
+                  Los pilotos podran correr con las ayudas que quieran.
+                </span>
+              </li>
+            </ul>
+          </TabPanel>
+          <TabPanel className="text-white" value={"pilotos"}>
+            <h1 className="pl-3 text-2xl">Pilotos Inscritos</h1>
+            <ul className="list-disc list-inside pl-3 mt-4">
+              <li>
+                <span className="relative right-2">Jose Marquez - Ferrari</span>
+              </li>
+              <li>
+                <span className="relative right-2">
+                  Andres Giusti - McLaren
+                </span>
+              </li>
+              <li>
+                <span className="relative right-2">
+                  Sebastian Villamizar - Ferrari
+                </span>
+              </li>
+              <li>
+                <span className="relative right-2">
+                  Jose Luis Peñaranda - Red Bull
+                </span>
+              </li>
+              <li>
+                <span className="relative right-2">
+                  Angel Martin - Por Definirse
+                </span>
+              </li>
+              <li>
+                <span className="relative right-2">
+                  Carlos Giusti - Por Definirse
+                </span>
+              </li>
+            </ul>
+          </TabPanel>
+          <TabPanel className="text-white text-2xl" value={"calendario"}>
+            <h1>El calendario esta por definirse</h1>
+          </TabPanel>
+          <TabPanel className="text-white text-2xl" value={"resultados"}>
+            <h1> No hay resultados para mostrar</h1>
+          </TabPanel>
+          <TabPanel className="text-white text-2xl" value={"estadisticas"}>
+            <h1> No hay estadisticas para mostrar</h1>
+          </TabPanel>
         </TabsBody>
       </Tabs>
     </>

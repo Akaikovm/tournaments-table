08d26774df40a025745d1b2213d8a7a8d5f16dcd
Sebastian <sebastianakaiko@gmail.com>
2023-03-02 14:45:40 -0500

    add new info

--- src/pages/live-tournaments/formula-one-tournament/formula-one-tournament.tsx
+++ src/pages/live-tournaments/formula-one-tournament/formula-one-tournament.tsx
@@ -49,6 +49,11 @@ const Formula1Tournament = () => {
               </li>
               <li>
                 <span className="relative right-2">
+                  Las Carreras tendran una distancia de 25%.
+                </span>
+              </li>
+              <li>
+                <span className="relative right-2">
                   De no poderse crear el modo liga desde el juego por tener
                   usuarios crossplay el torneo sera jugado en modo carrera
                   amistosa con las normativas correspondientes.
@@ -69,6 +74,13 @@ const Formula1Tournament = () => {
               <li>
                 <span className="relative right-2">
                   Los pilotos podran correr con las ayudas que quieran.
+                </span>
+              </li>
+              <li>
+                <span className="relative right-2">
+                  Si por alguna razon se cancela el torneo, quedaran como
+                  campeon y subcampeon los pilotos que de momento esten en 1er y
+                  2do lugar.
                 </span>
               </li>
             </ul>
@@ -96,18 +108,66 @@ const Formula1Tournament = () => {
               </li>
               <li>
                 <span className="relative right-2">
-                  Angel Martin - Por Definirse
+                  Angel Martin - Aston Martin
+                </span>
+              </li>
+              <li>
+                <span className="relative right-2">Carlos Giusti - Alpine</span>
+              </li>
+              <li>
+                <span className="relative right-2">Luis Garcia - Red Bull</span>
+              </li>
+            </ul>
+          </TabPanel>
+          <TabPanel className="text-white" value={"calendario"}>
+            <ul className="list-disc list-inside pl-3 mt-4">
+              <li>
+                <span className="relative right-2">GP De Baréin 5 Marzo</span>
+              </li>
+              <li>
+                <span className="relative right-2">
+                  GP De Arabia Saudita 19 Marzo
                 </span>
               </li>
               <li>
                 <span className="relative right-2">
-                  Carlos Giusti - Por Definirse
+                  GP De Australia 2 Abril
                 </span>
               </li>
+              <li>
+                <span className="relative right-2">
+                  GP De Azerbaiyan 30 Abril
+                </span>
+              </li>
+              <li>
+                <span className="relative right-2">GP De Miami 7 Mayo</span>
+              </li>
+              <li>
+                <span className="relative right-2">
+                  GP De Emilia-Romagna 21 Mayo
+                </span>
+              </li>
+              <li>
+                <span className="relative right-2">GP De Monaco 28 Mayo</span>
+              </li>
+              <li>
+                <span className="relative right-2">GP De España 4 Junio</span>
+              </li>
+              <li>
+                <span className="relative right-2">GP De Canada 18 Junio</span>
+              </li>
+              <li>
+                <span className="relative right-2">GP De Austria 2 Julio</span>
+              </li>
+              <li>
+                <span className="relative right-2">
+                  GP De Gran Bretaña 9 Julio
+                </span>
+              </li>
+              <li>
+                <span className="relative right-2">GP De Hungria 23 Julio</span>
+              </li>
             </ul>
-          </TabPanel>
-          <TabPanel className="text-white text-2xl" value={"calendario"}>
-            <h1>El calendario esta por definirse</h1>
           </TabPanel>
           <TabPanel className="text-white text-2xl" value={"resultados"}>
             <h1> No hay resultados para mostrar</h1>
